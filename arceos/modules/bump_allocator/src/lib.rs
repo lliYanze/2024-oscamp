72d48b37230d664cdb23ee5628c7ff9d08a39edb
alan li <alan@1310469009@qq.com>
2024-11-22 16:20:49 +0800

    [tmp] 未完成第二个作业

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 
+#[macro_use]
+extern crate axlog;
+
 use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 
 /// Early memory allocator
@@ -16,16 +19,96 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator;
-
-impl EarlyAllocator {
+pub struct EarlyAllocator<const PAGE_SIZE: usize> {
+    start: usize,
+    end: usize,
+    pagesize: usize,
 }
 
-impl BaseAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> EarlyAllocator<PAGE_SIZE> {
+    pub const fn new() -> Self {
+        // info!("EarlyAllocator::new()");
+        Self {
+            start: 0,
+            end: 0,
+            pagesize: PAGE_SIZE,
+        }
+    }
 }
 
-impl ByteAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> BaseAllocator for EarlyAllocator<PAGE_SIZE> {
+    fn init(&mut self, start: usize, size: usize) {
+        debug!("EarlyAllocator::init({:#x}, {:#x})", start, size);
+        self.start = start;
+        self.end = start + size;
+    }
+
+    fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
+        info!("EarlyAllocator::add_memory({:#x}, {:#x})", start, size);
+        allocator::AllocResult::Ok(())
+    }
 }
 
-impl PageAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> ByteAllocator for EarlyAllocator<PAGE_SIZE> {
+    fn alloc(
+        &mut self,
+        layout: core::alloc::Layout,
+    ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
+        info!("EarlyAllocator::alloc({:#x})", layout.size());
+        allocator::AllocResult::Ok(core::ptr::NonNull::dangling())
+    }
+
+    fn dealloc(&mut self, ptr: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
+        info!(
+            "EarlyAllocator::dealloc({:#x}), ptr is{:#x} ",
+            layout.size(),
+            ptr.as_ptr() as usize
+        );
+    }
+
+    fn total_bytes(&self) -> usize {
+        info!("EarlyAllocator::total_bytes()");
+        0
+    }
+
+    fn used_bytes(&self) -> usize {
+        info!("EarlyAllocator::used_bytes()");
+        0
+    }
+
+    fn available_bytes(&self) -> usize {
+        info!("EarlyAllocator::available_bytes()");
+        0
+    }
+}
+
+impl<const PAGE_SIZE: usize> PageAllocator for EarlyAllocator<PAGE_SIZE> {
+    const PAGE_SIZE: usize = PAGE_SIZE;
+    fn alloc_pages(
+        &mut self,
+        num_pages: usize,
+        align_pow2: usize,
+    ) -> allocator::AllocResult<usize> {
+        info!("EarlyAllocator::alloc_pages({}, {})", num_pages, align_pow2);
+        allocator::AllocResult::Ok(0)
+    }
+
+    fn dealloc_pages(&mut self, start: usize, count: usize) {
+        info!("EarlyAllocator::dealloc_pages({:#x}, {})", start, count);
+    }
+
+    fn total_pages(&self) -> usize {
+        info!("EarlyAllocator::total_pages()");
+        0
+    }
+
+    fn used_pages(&self) -> usize {
+        info!("EarlyAllocator::used_pages()");
+        0
+    }
+
+    fn available_pages(&self) -> usize {
+        info!("EarlyAllocator::available_pages()");
+        0
+    }
 }
--- arceos/ulib/axstd/src/collections/mod.rs
+++ arceos/ulib/axstd/src/collections/mod.rs
@@ -1,3 +1,4 @@
+extern crate alloc;
 pub use alloc::collections::*;
 
 pub use hashbrown::HashMap;
