d918589c7e5aa1a192a04352b942e5e929cd7e97
alan li <alan@1310469009@qq.com>
2024-11-29 18:45:00 +0800

    完成第二个作业

--- arceos/exercises/sys_map/src/syscall.rs
+++ arceos/exercises/sys_map/src/syscall.rs
@@ -1,13 +1,16 @@
 #![allow(dead_code)]
 
-use core::ffi::{c_void, c_char, c_int};
-use axhal::arch::TrapFrame;
-use axhal::trap::{register_trap_handler, SYSCALL};
+use alloc::vec::Vec;
+use arceos_posix_api as api;
 use axerrno::LinuxError;
+use axhal::arch::TrapFrame;
+use axhal::paging::MappingFlags;
+use axhal::trap::{register_trap_handler, SYSCALL};
 use axtask::current;
 use axtask::TaskExtRef;
-use axhal::paging::MappingFlags;
-use arceos_posix_api as api;
+use core::ffi::{c_char, c_int, c_void};
+use memory_addr::AddrRange;
+use memory_addr::VirtAddr;
 
 const SYS_IOCTL: usize = 29;
 const SYS_OPENAT: usize = 56;
@@ -100,9 +103,14 @@ bitflags::bitflags! {
 fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
     ax_println!("handle_syscall [{}] ...", syscall_num);
     let ret = match syscall_num {
-         SYS_IOCTL => sys_ioctl(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _) as _,
+        SYS_IOCTL => sys_ioctl(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _) as _,
         SYS_SET_TID_ADDRESS => sys_set_tid_address(tf.arg0() as _),
-        SYS_OPENAT => sys_openat(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _, tf.arg3() as _),
+        SYS_OPENAT => sys_openat(
+            tf.arg0() as _,
+            tf.arg1() as _,
+            tf.arg2() as _,
+            tf.arg3() as _,
+        ),
         SYS_CLOSE => sys_close(tf.arg0() as _),
         SYS_READ => sys_read(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
         SYS_WRITE => sys_write(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
@@ -110,11 +118,11 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
         SYS_EXIT_GROUP => {
             ax_println!("[SYS_EXIT_GROUP]: system is exiting ..");
             axtask::exit(tf.arg0() as _)
-        },
+        }
         SYS_EXIT => {
             ax_println!("[SYS_EXIT]: system is exiting ..");
             axtask::exit(tf.arg0() as _)
-        },
+        }
         SYS_MMAP => sys_mmap(
             tf.arg0() as _,
             tf.arg1() as _,
@@ -140,7 +148,56 @@ fn sys_mmap(
     fd: i32,
     _offset: isize,
 ) -> isize {
-    unimplemented!("no sys_mmap!");
+    ax_println!("mmap ...");
+    if !addr.is_null() {
+        ax_println!("addr is not null");
+        return -LinuxError::EINVAL.code() as _;
+    }
+    let range = AddrRange::<VirtAddr>::new(0.into(), 0x4FFFFFFF.into());
+
+    let va = match current()
+        .task_ext()
+        .aspace
+        .lock()
+        .find_free_area(0.into(), length, range)
+    {
+        Some(va) => va,
+        None => {
+            ax_println!("mmap failed: no free area");
+            return -LinuxError::ENOMEM.code() as _;
+        }
+    };
+    ax_println!("mmap va: {:#x?}", va);
+
+    let mut buffer = Vec::new();
+    buffer.resize(length, 0);
+    let size = sys_read(fd, buffer.as_mut_ptr() as *mut c_void, length as _);
+
+    ax_println!("mmap read size: {}", size);
+
+    let mmapflags = MmapFlags::from_bits_truncate(flags);
+    let mapflags = MappingFlags::USER | MappingFlags::READ | MappingFlags::WRITE;
+
+    ax_println!("mmap read buffer: {:?}", buffer);
+
+    match current()
+        .task_ext()
+        .aspace
+        .lock()
+        .map_alloc(va, 4096, mapflags, true)
+    {
+        Ok(_) => {
+            ax_println!("mmap success");
+        }
+        Err(e) => {
+            ax_println!("mmap failed: {:?}", e);
+            return -LinuxError::ENOMEM.code() as _;
+        }
+    }
+
+    current().task_ext().aspace.lock().write(va, &buffer);
+
+    va.as_usize() as _
 }
 
 fn sys_openat(dfd: c_int, fname: *const c_char, flags: c_int, mode: api::ctypes::mode_t) -> isize {
